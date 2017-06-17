10dd9fbec49641071588b3ab914a06434158209d
MH Rilwan <rilwan@gapstars.net>
2017-06-17 10:36:56 +0530

    :sparkles: event binding to show / hide image

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -5,8 +5,9 @@ import {Component} from '@angular/core';
 })
 export class ProductListComponent {
     pageTitle: string = 'Product List';
-    imageWidth: number = 50,
-    imageMargin: number = 2,
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
     products: any[] = [
         {
             "productId": 1,
@@ -29,4 +30,7 @@ export class ProductListComponent {
             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
         },
     ];
+    toggleImage(): void {
+        this.showImage = ! this.showImage;
+    }
 }
